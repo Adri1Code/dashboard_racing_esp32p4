269d6c9af1432f118451c52af5e0c6154e7cd9d0
Adrien Nicolas <103822035+AdrienBZH@users.noreply.github.com>
2026-01-16 16:33:22 +0100

    display_init.c and display_init.h

---
+++ components/display_init/display_init.c
@@ -0,0 +1,26 @@
+#include "display_init.h"
+#include "bsp/display.h"
+#include "bsp/esp-bsp.h"    
+
+// Configuration de l'affichage et initialisation du materiel
+void waveshare_display_init()
+{
+    bsp_display_cfg_t cfg = {
+        .lvgl_port_cfg = ESP_LVGL_PORT_INIT_CONFIG(),   // parametres de base pour LVGL
+        .buffer_size = BSP_LCD_DRAW_BUFF_SIZE,          // quantite de memoire allouee pour rendu graphique
+        .double_buffer = BSP_LCD_DRAW_BUFF_DOUBLE,      // rendu plus fluide
+        .flags = {
+            .buff_dma = true,                           // transferts memoire sans CPU
+            .buff_spiram = false,                       // utilise RAM interne plutot que SPIRAM
+            .sw_rotate = true,                          // pivot de l'affichage
+        }
+    };
+
+    lv_display_t *disp = bsp_display_start_with_config(&cfg);
+    bsp_display_backlight_on();
+    bsp_display_brightness_set(100);
+
+    if (disp != NULL) {
+        bsp_display_rotate(disp, LV_DISPLAY_ROTATION_180);
+    } 
+}
---
+++ components/display_init/include/display_init.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef DISPLAY_INIT_H
+#define DISPLAY_INIT_H
+
+void waveshare_display_init();
+
+#endif
