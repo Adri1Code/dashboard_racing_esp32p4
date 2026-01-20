95fa8783c2725ad8853c3b6459211323c0dcf5d4
Adrien Nicolas <103822035+AdrienBZH@users.noreply.github.com>
2026-01-20 13:10:50 +0100

    analysis of RAM PSRAM and FLASH

--- main/main.c
+++ main/main.c
@@ -1,6 +1,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_heap_caps.h"
+#include "esp_partition.h"
+#include "esp_flash.h"
 #include "esp_log.h"
+#include "spi_flash_mmap.h"
 #include "bsp/esp-bsp.h"    
 #include "bsp/display.h"
 #include "lvgl.h"
@@ -17,6 +22,39 @@ void app_main(void)
     ESP_LOGI(TAG, "Debut du programme");
 
     waveshare_display_init();
+
+    // INFOS RAM + INFOS FLASH
+    ESP_LOGI(TAG, "============ ANALYSE RAM ============");
+    size_t free_ram_internal = heap_caps_get_free_size( MALLOC_CAP_INTERNAL );
+    size_t total_ram_internal = heap_caps_get_total_size( MALLOC_CAP_INTERNAL );
+    ESP_LOGI(TAG, "RAM interne : Libre: %d Ko / Totale: %d Ko", free_ram_internal / 1024 , total_ram_internal / 1024);
+
+    ESP_LOGI(TAG, "============ ANALYSE PSRAM ============");
+    size_t free_psram_internal = heap_caps_get_free_size( MALLOC_CAP_SPIRAM );
+    size_t total_psram_internal = heap_caps_get_total_size( MALLOC_CAP_SPIRAM );
+    if(total_psram_internal > 0)
+    {
+        ESP_LOGI(TAG, "PSRAM (EXTERNE) : Libre: %d Ko / Totale: %d Ko", free_psram_internal / 1024, total_psram_internal / 1024);
+    }else{
+        ESP_LOGW(TAG, "PSRAM non detectee"); 
+    }
+
+    ESP_LOGI(TAG, "============ ANALYSE FLASH ============");
+    uint32_t flash_size;
+    esp_flash_get_size(NULL, &flash_size);
+    ESP_LOGI(TAG, "Taille de la FLASH : %lu Mo", flash_size / 1024 * 1024);
+
+    ESP_LOGI(TAG, "Detail du fichier partitions.csv");
+    esp_partition_iterator_t partition_iterator = esp_partition_find(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, NULL);
+
+    while(partition_iterator != NULL)
+    {
+        const esp_partition_t *p = esp_partition_get(partition_iterator);
+        ESP_LOGI(TAG, "- Partition '%s' | Type: 0x%x | Taille: %lu Ko", p->label, p->type, p->size / 1024);
+        partition_iterator = esp_partition_next(partition_iterator);
+    }
+
+    esp_partition_iterator_release(partition_iterator);
 
     DIR *dir = sd_card_mount();
 
