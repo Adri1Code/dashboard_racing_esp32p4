2b6c3cbb4f2c577d8d48ab13284d80384c4b3b96
Adrien Nicolas <103822035+AdrienBZH@users.noreply.github.com>
2026-01-21 10:59:58 +0100

    save and read slider brightness value to NVS

--- components/slider/slider.c
+++ components/slider/slider.c
@@ -1,8 +1,37 @@
 #include "slider.h"
 #include "esp_log.h"  
 #include "bsp/display.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+
+#define STORAGE_NAMESPACE "storage"
 
 static const char *TAG = "SLIDER";
+
+// Charger une valeur en NVS
+void save_brightness_to_nvs(uint32_t value)
+{
+    nvs_handle_t my_handle;
+    if (nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle) == ESP_OK)
+    {
+        nvs_set_u32(my_handle, "brightness", value);
+        nvs_commit(my_handle);  // valider l'ecriture
+        nvs_close(my_handle);
+    }
+}
+
+// Lire une valeur en NVS
+uint32_t load_brightness_from_nvs()
+{
+    nvs_handle_t my_handle;
+    uint32_t value = 100;
+    if (nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &my_handle) == ESP_OK)
+    {
+        nvs_get_u32(my_handle, "brightness", &value);
+        nvs_close(my_handle);
+    }
+    return value;
+}
 
 // Modifie la luminosite selon le changement sur le slider
 void brightness_slider_event_cb(lv_event_t *event)
@@ -11,6 +40,7 @@ void brightness_slider_event_cb(lv_event_t *event)
     uint32_t value = lv_slider_get_value( slider );
 
     bsp_display_brightness_set( value );
+    save_brightness_to_nvs( value );
 
     ESP_LOGI(TAG, "Luminositee ajustee a : %ld%%", value);
 }
@@ -18,11 +48,16 @@ void brightness_slider_event_cb(lv_event_t *event)
 // Creation d'un slider avec reglage de la luminosite
 void slider_brightness_cfg(lv_obj_t *screen)
 {
+    uint32_t saved_value = load_brightness_from_nvs();
+
+    bsp_display_brightness_set( saved_value );
+
     lv_obj_t *slider = lv_slider_create( screen );       // creer un slider
-    lv_slider_set_range(slider, 0, 100);                // range du slider
-    lv_slider_set_value(slider, 100, LV_ANIM_OFF);      // valeur initiale
-    lv_obj_set_width(slider, lv_pct(80));               // 80% de la largeur de l'ecran
-    lv_obj_align(slider, LV_ALIGN_TOP_MID, 0, 20);      // aligne en haut au centre
+    lv_slider_set_range(slider, 0, 100);                 // range du slider
+    lv_slider_set_value(slider, saved_value, LV_ANIM_OFF);
+
+    lv_obj_set_width(slider, lv_pct(80));                // 80% de la largeur de l'ecran
+    lv_obj_align(slider, LV_ALIGN_TOP_MID, 0, 20);       // aligne en haut au centre
 
     // Evenement : utilisateur change le slider 
     lv_obj_add_event_cb(slider, brightness_slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);
--- main/main.c
+++ main/main.c
@@ -3,6 +3,7 @@
 #include "bsp/esp-bsp.h"    
 #include "bsp/display.h"
 #include "lvgl.h"
+#include "nvs_flash.h"
 #include "display_init.h"
 #include "sd_card.h"
 #include "slider.h"
@@ -15,6 +16,14 @@ static const char *TAG = "APP_MAIN";
 
 void app_main(void)
 { 
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
     LOG_BREAK;
     ESP_LOGI(TAG, "Debut du programme");
 
